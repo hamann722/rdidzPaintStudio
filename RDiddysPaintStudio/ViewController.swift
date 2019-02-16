80c4c149f8179f51fc6a8049e35a22f5542e4f74
Rohit Didwania <rdwania@gmail.com>
2019-02-16 13:35:46 -0500

    updated colors

--- RDiddysPaintStudio/ViewController.swift
+++ RDiddysPaintStudio/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     private var animationDuration = 0.2
-    private let colors = [UIColor.init(red: 197.0/255, green: 187.0/255, blue: 161.0/255, alpha: 1.0), UIColor.init(red: 66.0/255, green: 34.0/255, blue: 28.0/255, alpha: 1), UIColor.init(red: 84.0/255, green: 61.0/255, blue: 38.0/255, alpha: 1), UIColor.black, UIColor.white, UIColor.darkGray, UIColor.init(red: 70.0/255, green: 22.0/255, blue: 79.0/255, alpha: 1.0), UIColor.init(red: 62.0/255, green: 13.0/255, blue: 90.0/255, alpha: 1), UIColor.init(red: 137.0/255, green: 33.0/255, blue: 118.0/255, alpha: 1), UIColor.init(red: 181.0/255, green: 100/255.0, blue: 165.0/255, alpha: 1), UIColor.init(red: 166.0/255, green: 136.0/255, blue: 178.0/255, alpha: 1), UIColor.init(red: 203.0/255, green: 192.0/255, blue: 221.0/255, alpha: 1), UIColor.init(red: 141.0/255, green: 164.0/255, blue: 208.0/255, alpha: 1), UIColor.init(red: 0, green: 80.0/255, blue: 135.0/255, alpha: 1), UIColor.init(red: 0, green: 88.0/255, blue: 174.0/255, alpha: 1), UIColor.init(red: 0, green: 148.0/255, blue: 186.0/255, alpha: 1), UIColor.init(red: 51.0/255, green: 169.0/255, blue: 189.0/255, alpha: 1), UIColor.init(red: 153.0/255, green: 216.0/255, blue: 220.0/255, alpha: 1), UIColor.init(red: 0, green: 124.0/255, blue: 80.0/255, alpha: 1), UIColor.init(red: 90.0/255, green: 129.0/255, blue: 54.0/255, alpha: 1), UIColor.init(red: 64.0/255, green: 186.0/255, blue: 104.0/255, alpha: 1), UIColor.init(red: 121.0/255, green: 197.0/255, blue: 76.0/255, alpha: 1), UIColor.init(red: 149.0/255, green: 201.0/255, blue: 147.0/255, alpha: 1), UIColor.init(red: 205.0/255, green: 230.0/255, blue: 173.0/255, alpha: 1), UIColor.init(red: 180.0/255, green: 145.0/255, blue: 104.0/255, alpha: 1), UIColor.init(red: 223.0/255, green: 185.0/255, blue: 0, alpha: 1), UIColor.init(red: 248.0/255, green: 150.0/255, blue: 0, alpha: 1), UIColor.init(red: 251.0/255, green: 163.0/255, blue: 100.0/255, alpha: 1), UIColor.init(red: 244.0/255, green: 96.0/255, blue: 58.0/255, alpha: 1), UIColor.init(red: 211.0/255, green: 82.0/255, blue: 12.0/255, alpha: 1), UIColor.init(red: 176.0/255, green: 0, blue: 119.0/255, alpha: 1), UIColor.init(red: 235.0/255, green: 0, blue: 138.0/255, alpha: 1), UIColor.init(red: 202.0/255, green: 51.0/255, blue: 102.0/255, alpha: 1), UIColor.init(red: 249.0/255, green: 163.0/255, blue: 192.0/255, alpha: 1), UIColor.init(red: 240.0/255, green: 116.0/255, blue: 102.0/255, alpha: 1), UIColor.init(red: 252.0/255, green: 208.0/255, blue: 191.0/255, alpha: 1)]
-    
+    private var colors = [UIColor.init(r: 0, g: 0, b: 0), UIColor.init(r: 255, g: 255, b: 255), UIColor.init(r: 255, g: 192, b: 203), UIColor.init(r: 0, g: 128, b: 128), UIColor.init(r: 255, g: 228, b: 225), UIColor.init(r: 255, g: 0, b: 0), UIColor.init(r: 255, g: 215, b: 0), UIColor.init(r: 211, g: 255, b: 206), UIColor.init(r: 0, g: 255, b: 255),UIColor.init(r: 64, g: 224, b: 208), UIColor.init(r: 255, g: 115, b: 115), UIColor.init(r: 230, g: 230, b: 250), UIColor.init(r: 0, g: 0, b: 255),UIColor.init(r: 240, g: 248, b: 255), UIColor.init(r: 255, g: 165, b: 0), UIColor.init(r: 176, g: 224, b: 230), UIColor.init(r: 238, g: 238, b: 238), UIColor.init(r: 204, g: 204, b: 204), UIColor.init(r: 127, g: 255, b: 212), UIColor.init(r: 51, g: 51, b: 51), UIColor.init(r: 192, g: 192, b: 192), UIColor.init(r: 0, g: 51, b: 102),UIColor.init(r: 128, g: 0, b: 128),UIColor.init(r: 128, g: 0, b: 0), UIColor.init(r: 102, g: 205, b: 170), UIColor.init(r: 25, g: 25, b: 112), UIColor.init(r: 204, g: 255, b: 0), UIColor.init(r: 51, g: 153, b: 255), UIColor.init(r: 255, g: 127, b: 80), UIColor.init(r: 246, g: 84, b: 106)]
+
     @IBOutlet var colorCollectionViewConstraint : NSLayoutConstraint!
     @IBOutlet weak var colorCollectionView : UICollectionView!
 
@@ -46,7 +46,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 36
+        return self.colors.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -99,4 +99,9 @@ class ColorCell : UICollectionViewCell{
         }
     }
     
+}
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
 }
